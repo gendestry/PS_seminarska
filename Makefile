2c91b3f3703b58fdbb31f02ccf4dfbe0834f1046
MrTipson <gunfight7.gf@gmail.com>
2022-02-04 17:32:30 +0100

    Add OpenMP graph implementation

--- Makefile
+++ Makefile
@@ -6,8 +6,13 @@ test: Parser.cpp test.cpp
 	g++ Parser.cpp test.cpp -g --std=c++17 -o test
 
 run: test
-	srun -n1 --cpus-per-task=1 --reservation=fri ./test
-	#./test
+	srun --reservation=fri ./test
+
+omp: Parser.cpp PageRank_omp.cpp
+	g++ Parser.cpp PageRank_omp.cpp -fopenmp -g --std=c++17 -o omp
+
+run_omp: omp
+	srun -n1 --cpus-per-task=32 --reservation=fri ./omp
 
 clean:
 	rm test
---
+++ PageRank_omp.cpp
@@ -0,0 +1,88 @@
+#include "Parser.h"
+#include "omp.h"
+
+
+template<typename T>
+void graphIteration(std::string path);
+
+template<typename T>
+void matrixIteration(std::string path);
+
+int main(int argc, char** argv) {
+    graphIteration<double>("graph-google.txt");
+    //matrixIteration<double>("graph-google.txt");
+
+    return 0;
+}
+
+template<typename T>
+void graphIteration(std::string path) {
+    auto nodes = Parser::getNodes<T>(path);
+
+    // initial setup
+    const int N = nodes.size();
+    const T initRank = 1.0 / N;
+    const T d = 0.85;
+    const T offset = (1 - d) / N;
+
+	std::vector<unsigned int> keys(nodes.size());
+	int count = 0;
+    for (auto& [id, node] : nodes) {
+		keys[count++] = id;
+        node->rank = initRank;
+        node->prevRank = initRank;
+    }
+    // start iterating
+    T temp;
+    T rankDiff = 1.0;
+    const T err = 1e-5;
+
+    for (int counter = 0; rankDiff > err; counter++) {
+        #pragma omp parallel for
+		for(int i = 0; i < keys.size(); i++){
+			auto node = nodes[keys[i]];
+            node->prevRank = node->rank;
+		}
+
+        rankDiff = 0.0;
+		#pragma omp parallel for reduction(+:rankDiff) private(temp)
+        for (int i = 0; i < keys.size(); i++) {
+			auto node = nodes[keys[i]];
+            temp = 0.0;
+            for (auto& child : node->links) {
+                temp += child->prevRank / child->outCount;
+            }
+            node->rank = offset + d * temp;
+            rankDiff += fabs(node->rank - node->prevRank);
+            //std::cout << "[" << counter << "] " << node->id << ": " << node->rank << std::endl;
+        }
+        std::cout << "[" << counter << "] " << rankDiff << std::endl;
+    }
+}
+
+template<typename T>
+void matrixIteration(std::string path) {
+    auto matrixInfo = Parser::getMatrix<T>(path);
+    auto& M = matrixInfo.matrix;
+
+    const int N = matrixInfo.idMap.size();
+    const T initRank = 1.0 / N;
+    const T d = 0.85;
+    const T offset = (1 - d) / N;
+
+    Matrix<T> rank(N, 1, initRank);
+    Matrix<T> prevRank(N, 1, initRank);
+    Matrix<T> ones(N, 1, offset);
+
+    T rankDiff = 1.0;
+    const T err = 1e-5;
+
+    for (int counter = 0; rankDiff > err; counter++) {
+        rankDiff = 0.0;
+        prevRank = rank;
+        rank = ((M * prevRank) * d) + ones;
+        rankDiff = (rank - prevRank).abs().sum();
+        //std::cout << "--- [" << counter << "] ---" << std::endl << rank;
+        std::cout << "[" << counter << "] " << rankDiff << std::endl;
+    }
+}
